fa7084d5b9493bc6f7b0ecce41ed6b290df851eb
Ryan Hallisey <rhallise@redhat.com>
2015-02-27 12:51:30 -0500

    Change docker pull to atomic install

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@
 #cp -p glance.conf ${HOST}/etc/glance/glance.conf
 
 # Pull Kolla Containers (can be replaced with atomic install <container>)
-chroot ${HOST} docker pull kollaglue/fedora-rdo-rabbitmq
-chroot ${HOST} docker pull kollaglue/fedora-rdo-mariadb
-chroot ${HOST} docker pull kollaglue/fedora-rdo-keystone
+chroot ${HOST} atomic install kollaglue/fedora-rdo-rabbitmq
+chroot ${HOST} atomic install kollaglue/fedora-rdo-mariadb
+chroot ${HOST} atomic install kollaglue/fedora-rdo-keystone
