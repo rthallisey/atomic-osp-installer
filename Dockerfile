0351600d6f1f16697710b0e668a957ccc3baf0f6
Ryan Hallisey <rhallise@redhat.com>
2015-03-13 14:23:20 -0400

    Combine getenv.sh and atomic-install.sh.
    Changes to get atomic to install openstack.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM fedora
 MAINTAINER Ryan Hallisey <rhallise@redhat.com>
 ENV container openstack
-RUN yum update -y; yum -y install mariadb openstack-keystone openstack-nova-compute openstack-glance iproute; yum -y clean all
+RUN yum update -y; yum -y install docker mariadb openstack-keystone openstack-nova-compute openstack-glance iproute; yum -y clean all
 
-LABEL INSTALL="docker run --rm --privileged --net=host -v /:/host --env-file=openstack.env -e HOST=/host -e IMAGE=IMAGE -e NAME=NAME --name NAME IMAGE /usr/bin/atomic-start.sh"
+LABEL INSTALL="docker run --rm --privileged --net=host -v /:/host -v /var/lib:/var/lib -v /run:/run --env-file=openstack.env -e HOST=/host -e IMAGE=IMAGE -e NAME=NAME --name NAME IMAGE /usr/bin/atomic-start.sh"
 
 ADD atomic-start.sh /usr/bin/atomic-start.sh
-ADD openstack.env /etc/openstack.env
-ADD openrc /etc/openrc
--- atomic-start.sh
+++ atomic-start.sh
@@ -1,49 +1,142 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Commenting out for now.  DB will just get remade each time in the container.
 #rm -rf /var/lib/mysql
 #mkdir -p /var/lib/mysql
 
+export HOME=/root
+
 #setenforce 0
-modprobe ebtables
+chroot ${HOST} modprobe ebtables
 
 #systemctl stop libvirtd
 
 #firewall-cmd --add-service=mysql
 
 # Cleanup from previous runs.  Just for iteration purposes for now.
-#echo "Stopping any running openstack containers.."
-#containers=`docker ps -q`
-#if [ ! -z "$containers" ]; then
-#    docker ps -q | xargs docker stop
-#fi
-
-#echo "Removing any running openstack containers.."
-#containers=`docker ps -qa`
-#if [ ! -z "$containers" ]; then
-#    docker ps -qa | xargs docker rm
-#fi
+echo "Removing any running openstack containers.."
+containers=`docker ps -qa`
+if [ ! -z "$containers" ]; then
+    docker ps -qa | xargs docker rm -f
+fi
 
 # We need these for the user setup commands.
 #yum -y install openstack-keystone openstack-glance openstack-nova mariadb
 
 MY_IP=$(ip route get $(ip route | awk '$1 == "default" {print $3}') |
     awk '$4 == "src" {print $5}')
+MY_DEV=$(ip route get $(ip route | awk '$1 == "default" {print $3}') |
+    awk '$4 == "src" {print $3}')
 
-cp /etc/openstack.env ${HOST}/etc/openstack.env
+echo MY_IP=$MY_IP
+echo MY_DEV=$MY_DEV
+
+# Database
+HOST_IP=$MY_IP
+MYSQL_ROOT_PASSWORD=kolla
+PASSWORD=12345
+
+# Host
+ADMIN_TENANT_NAME=admin
+PUBLIC_IP=$HOST_IP
+
+# RabbitMQ
+RABBITMQ_SERVICE_HOST=$HOST_IP
+RABBIT_USER=guest
+RABBIT_PASSWORD=guest
+
+# Keystone
+KEYSTONE_ADMIN_TOKEN=$PASSWORD
+KEYSTONE_DB_PASSWORD=kolla
+KEYSTONE_ADMIN_PASSWORD=$PASSWORD
+KEYSTONE_PUBLIC_SERVICE_HOST=$HOST_IP
+KEYSTONE_ADMIN_SERVICE_HOST=$HOST_IP
+KEYSTONE_AUTH_PROTOCOL=http
+
+# Glance
+GLANCE_DB_NAME=glance
+GLANCE_DB_USER=glance
+GLANCE_DB_PASSWORD=kolla
+GLANCE_KEYSTONE_USER=glance
+GLANCE_KEYSTONE_PASSWORD=glance
+GLANCE_API_SERVICE_HOST=$HOST_IP
+
+# Nova
+NOVA_DB_PASSWORD=nova
+NOVA_DB_NAME=nova
+NOVA_DB_USER=nova
+NOVA_KEYSTONE_USER=nova
+NOVA_KEYSTONE_PASSWORD=nova
+NOVA_API_SERVICE_HOST=$HOST_IP
+NOVA_EC2_SERVICE_HOST=$HOST_IP
+NOVA_PUBLIC_INTERFACE=$MY_DEV
+NOVA_FLAT_INTERFACE=$MY_DEV
+CONFIG_NETWORK=True
+
+cat > /etc/openrc <<EOF
+export OS_AUTH_URL="http://${KEYSTONE_PUBLIC_SERVICE_HOST}:5000/v2.0"
+export OS_USERNAME=$ADMIN_TENANT_NAME
+export OS_PASSWORD=$PASSWORD
+export OS_TENANT_NAME=$ADMIN_TENANT_NAME
+EOF
+
+cat > /etc/openstack.env <<EOF
+ADMIN_TENANT_NAME=$ADMIN_TENANT_NAME
+CONFIG_NETWORK=$CONFIG_NETWORK
+DB_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
+FLAT_INTERFACE=$NOVA_FLAT_INTERFACE
+GLANCE_API_SERVICE_HOST=$GLANCE_API_SERVICE_HOST
+GLANCE_DB_NAME=$GLANCE_DB_NAME
+GLANCE_DB_PASSWORD=$GLANCE_DB_PASSWORD
+GLANCE_DB_USER=$GLANCE_DB_USER
+GLANCE_KEYSTONE_PASSWORD=$GLANCE_KEYSTONE_PASSWORD
+GLANCE_KEYSTONE_USER=$GLANCE_KEYSTONE_USER
+KEYSTONE_ADMIN_PASSWORD=$KEYSTONE_ADMIN_PASSWORD
+KEYSTONE_ADMIN_SERVICE_HOST=$KEYSTONE_ADMIN_SERVICE_HOST
+KEYSTONE_ADMIN_SERVICE_PORT=5000
+KEYSTONE_ADMIN_TOKEN=$KEYSTONE_ADMIN_TOKEN
+KEYSTONE_AUTH_PROTOCOL=$KEYSTONE_AUTH_PROTOCOL
+KEYSTONE_DB_PASSWORD=$KEYSTONE_DB_PASSWORD
+KEYSTONE_PUBLIC_SERVICE_HOST=$KEYSTONE_PUBLIC_SERVICE_HOST
+MARIADB_SERVICE_HOST=$HOST_IP
+MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
+NETWORK_MANAGER=nova
+NOVA_API_SERVICE_HOST=$NOVA_API_SERVICE_HOST
+NOVA_DB_NAME=$NOVA_DB_NAME
+NOVA_DB_PASSWORD=$NOVA_DB_PASSWORD
+NOVA_DB_USER=$NOVA_DB_USER
+NOVA_EC2_API_SERVICE_HOST=$NOVA_EC2_SERVICE_HOST
+NOVA_EC2_SERVICE_HOST=$NOVA_EC2_SERVICE_HOST
+NOVA_KEYSTONE_PASSWORD=$NOVA_KEYSTONE_PASSWORD
+NOVA_KEYSTONE_USER=$NOVA_KEYSTONE_USER
+PUBLIC_INTERFACE=$NOVA_PUBLIC_INTERFACE
+PUBLIC_IP=$HOST_IP
+PUBLIC_IP=$PUBLIC_IP
+RABBITMQ_PASS=$RABBIT_PASSWORD
+RABBITMQ_SERVICE_HOST=$RABBITMQ_SERVICE_HOST
+RABBITMQ_USER=$RABBIT_USER
+RABBIT_PASSWORD=$RABBIT_PASSWORD
+RABBIT_USERID=$RABBIT_USER
+EOF
+
 # Source openrc for commands
 source /etc/openrc
+cp /etc/openstack.env ${HOST}/etc/openstack.env
 
 ######## RABBITMQ ########
 echo Starting rabbitmq
-# chroot ${HOST} atomic install fedora-rdo-rabbitmq-atomic
-chroot ${HOST} docker run -d --name rabbitmq -p 5672:5672 --env-file=/etc/openstack.env imain/fedora-rdo-rabbitmq
+# atomic install fedora-rdo-rabbitmq-atomic
+docker run -d --name rabbitmq -p 5672:5672 --env-file=/etc/openstack.env imain/fedora-rdo-rabbitmq
 
 ######## MARIADB ########
 echo Starting mariadb
-chroot ${HOST} docker run -d --name mariadb -p 3306:3306 --env-file=/etc/openstack.env imain/fedora-rdo-mariadb
+#rm -rf /var/lib/mysql
+mkdir -p ${HOST}/var/lib/mysql
+mkdir -p ${HOST}/var/log/mariadb
 
-until mysql -u root --password=kolla --host=$MY_IP mysql -e "show tables;" 2> /dev/null
+docker run -d --name mariadb --net=host -v /var/lib/mysql:/var/lib/mysql:Z -v /var/log/mariadb:/var/log/mariadb:Z --env-file=/etc/openstack.env imain/fedora-rdo-mariadb
+
+until mysql -u root --password=kolla mysql -e "show tables;"
 do
     echo waiting for mysql..
     sleep 3
@@ -51,9 +144,9 @@ done
 
 ######## KEYSTONE ########
 echo Starting keystone
-chroot ${HOST} docker run -d --name keystone -p 5000:5000 -p 35357:35357 \
+docker run -d --name keystone --net=host \
        --env-file=/etc/openstack.env imain/fedora-rdo-keystone
-until keystone user-list 2> /dev/null
+until keystone user-list
 do
     echo waiting for keystone..
     sleep 3
@@ -61,20 +154,20 @@ done
 
 ######## GLANCE ########
 echo Starting glance
-chroot ${HOST} docker run --name glance-registry -p 9191:9191 -d \
+docker run --name glance-registry --net=host -d \
        --env-file=/etc/openstack.env rthallisey/fedora-rdo-glance-registry:latest
 
-chroot ${HOST} docker run --name glance-api -d -p 9292:9292 \
+docker run --name glance-api -d --net=host \
        --link glance-registry:glance-registry \
        --link rabbitmq:rabbitmq \
        --env-file=/etc/openstack.env rthallisey/fedora-rdo-glance-api:latest
 
 ######## NOVA ########
 echo Starting nova-conductor
-chroot ${HOST} docker run --name nova-conductor -d \
+docker run --name nova-conductor -d --net=host\
        --env-file=/etc/openstack.env imain/fedora-rdo-nova-conductor:latest
 
-until mysql -u root --password=kolla --host=$MY_IP mysql -e "use nova;" 2> /dev/null
+until mysql -u root --password=kolla --host=$MY_IP mysql -e "use nova;"
 do
     echo waiting for nova-conductor to create the database..
     sleep 3
@@ -84,10 +177,10 @@ done
 # it is running an iptables command which fails because it doesn't have
 #permissions.
 echo Starting nova-api
-chroot ${HOST} docker run --name nova-api -d --privileged -p 8774:8774 \
+docker run --name nova-api -d --privileged --net=host \
        --env-file=/etc/openstack.env imain/fedora-rdo-nova-api:latest
 
-until keystone user-list | grep nova 2> /dev/null
+until keystone user-list | grep nova
 do
     echo waiting for nova-api to create the keystone nova user..
     sleep 2
@@ -95,7 +188,7 @@ done
 
 # This directory is shared with the host to allow qemu instance
 # configs to remain accross restarts.
-chroot ${HOST} mkdir -p /etc/libvirt/qemu
+ mkdir -p /etc/libvirt/qemu
 
 # Libvirt is in nova compute for now.
 #echo Starting libvirt
@@ -106,7 +199,7 @@ chroot ${HOST} mkdir -p /etc/libvirt/qemu
 #	kollaglue/fedora-rdo-nova-libvirt
 
 echo Starting nova compute
-chroot ${HOST} docker run -d --privileged \
+docker run -d --privileged \
        -v /sys/fs/cgroup:/sys/fs/cgroup \
        -v /var/lib/nova:/var/lib/nova \
        -v /run/libvirt:/run/libvirt \
@@ -115,12 +208,12 @@ chroot ${HOST} docker run -d --privileged \
        --env-file=/etc/openstack.env imain/fedora-rdo-nova-compute:latest
 
 echo Starting nova-network
-chroot ${HOST} docker run --name nova-network -d --privileged \
+docker run --name nova-network -d --privileged \
        --net=host \
        --env-file=/etc/openstack.env imain/fedora-rdo-nova-network:latest
 
 echo Starting nova-scheduler
-chroot ${HOST} docker run --name nova-scheduler -d \
+docker run --name nova-scheduler -d --net=host \
        --env-file=/etc/openstack.env imain/fedora-rdo-nova-scheduler:latest
 
 IMAGE_URL=http://download.cirros-cloud.net/0.3.3/
