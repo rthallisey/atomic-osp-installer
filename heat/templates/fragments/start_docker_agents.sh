9061852ccf35896efae0474b1234ec823d1481ce
Ian Main <imain@redhat.com>
2015-03-26 15:19:39 -0400

    Don't want to use overlay, doesn't help and seems flakey

--- heat/templates/fragments/start_docker_agents.sh
+++ heat/templates/fragments/start_docker_agents.sh
@@ -28,7 +28,7 @@ EOF
 # For older docker versions < 1.4.x use commented line
 #sed -i '/^OPTIONS/s/$/ --insecure-registry 192.168.1.102:5000/' /etc/sysconfig/docker
 #sed -i "/INSECURE_REGISTRY/c\INSECURE_REGISTRY='--insecure-registry 10.18.57.5:8080'" /etc/sysconfig/docker
-echo "OPTIONS='-s overlay --insecure-registry $docker_registry --selinux-enabled'" >> /etc/sysconfig/docker
+echo "OPTIONS='--insecure-registry $docker_registry --selinux-enabled'" >> /etc/sysconfig/docker
 echo "ADD_REGISTRY='--add-registry $docker_registry'" >> /etc/sysconfig/docker
 
 # enable and start docker
