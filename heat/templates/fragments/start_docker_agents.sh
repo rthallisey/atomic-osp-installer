31df9e0bc226927e4d00255880c0f43ee3d5bcc6
Ian Main <imain@redhat.com>
2015-03-27 17:03:10 -0400

    Add libvirt to template.
    Add an attempt to download the base image and load it into docker for
    performance reasons.

--- heat/templates/fragments/start_docker_agents.sh
+++ heat/templates/fragments/start_docker_agents.sh
@@ -14,6 +14,8 @@ User=root
 Restart=on-failure
 ExecStartPre=-/usr/bin/docker kill heat-agents
 ExecStartPre=-/usr/bin/docker rm heat-agents
+ExecStartPre=-/bin/curl http://shell.bos.redhat.com/~rhallise/docker-tarfiles/centos-rdo-base.tar | /usr/bin/docker load
+
 ExecStartPre=/usr/bin/docker pull $agent_image
 ExecStart=/usr/bin/docker run --name heat-agents --privileged --net=host -v /etc:/host/etc -v /usr/bin/atomic:/usr/bin/atomic -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/cloud:/var/lib/cloud -v /var/lib/heat-cfntools:/var/lib/heat-cfntools $agent_image
 ExecStop=/usr/bin/docker stop heat-agents
@@ -26,10 +28,10 @@ EOF
 # update docker for local insecure registry(optional)
 # Note: This is different for different docker versions
 # For older docker versions < 1.4.x use commented line
-#sed -i '/^OPTIONS/s/$/ --insecure-registry 192.168.1.102:5000/' /etc/sysconfig/docker
-#sed -i "/INSECURE_REGISTRY/c\INSECURE_REGISTRY='--insecure-registry 10.18.57.5:8080'" /etc/sysconfig/docker
 echo "OPTIONS='--insecure-registry $docker_registry --selinux-enabled'" >> /etc/sysconfig/docker
 echo "ADD_REGISTRY='--add-registry $docker_registry'" >> /etc/sysconfig/docker
+
+/sbin/setenforce 0
 
 # enable and start docker
 /usr/bin/systemctl enable docker.service
