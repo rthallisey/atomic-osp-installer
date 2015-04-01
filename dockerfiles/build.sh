1a4a6f08b1fd738b8bcc5e26f13d7ac5a8eb70e5
Ian Main <imain@redhat.com>
2015-04-01 13:15:39 -0400

    Generate a pull.sh for caching onto the image.

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 
+rm pull.sh
+
 for i in `ls -1 Dockerfile.*`; do
   extention=`echo $i | cut -d'.' -f2`
   docker build -f $i -t imain/atomic-centos-rdo-$extention:latest .
   docker push imain/atomic-centos-rdo-$extention:latest
+  echo docker pull imain/atomic-centos-rdo-$extention:latest >> pull.sh
 done
